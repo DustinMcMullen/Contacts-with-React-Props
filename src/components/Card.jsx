e2b88c9bdf0ce6ee3e8f9a10a92ce2f98793a31b
DustinMcMullen <51675499+DustinMcMullen@users.noreply.github.com>
2020-08-29 10:09:17 -0500

    Further abstracted with components Created Info component to house contact details: phone & email

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Avatar from "./Avatar";
+import Info from "./Info";
 
 function Card(props) {
   return (
@@ -6,11 +8,10 @@ function Card(props) {
       <div className="card">
         <div className="top">
           <h2 className="name">{props.name}</h2>
-          <img className="circle-img" src={props.image} alt="avatar_img" />
+          <Avatar image={props.image} />
         </div>
         <div className="bottom">
-          <p className="info">{props.phone}</p>
-          <p className="info">{props.email}</p>
+          <Info phone={props.phone} email={props.email} />
         </div>
       </div>
     </div>
