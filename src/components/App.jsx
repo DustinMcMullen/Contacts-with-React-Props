7ace6a50a2f345fb5d8892e74821578bc62bae6a
DustinMcMullen <51675499+DustinMcMullen@users.noreply.github.com>
2020-08-29 02:24:00 -0500

    Added components & props created modular Contact Card & implemented 3 iterations.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,28 @@
 import React from "react";
+import Card from "./Card";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2>Beyonce</h2>
-          <img
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
-        </div>
-      </div>
+      <Card
+        name="Beyonce"
+        image="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        phone="123-456-7890"
+        email="b@singleladies.com"
+      />
+      <Card
+        name="Nas"
+        image="https://www.biography.com/.image/c_fill%2Ccs_srgb%2Cfl_progressive%2Ch_400%2Cq_auto:good%2Cw_620/MTIwNjA4NjMzNjkyMzI1Mzg4/nas-507692-1-402.jpg"
+        phone="098-765-4321"
+        email="nastynas@illmatic.com"
+      />
+      <Card
+        name="John Wayne"
+        image="https://pmcvariety.files.wordpress.com/2020/06/john-wayne.jpg"
+        phone="111-222-3333"
+        email="Marion@westerns.com"
+      />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-//1. Apply CSS styles to App.jsx component
-//to match the appearance on the completed app:
-//https://c6fkx.csb.app/
-//2. Extract the contact card as a reusable Card component.
-//3. Use props to render the default Beyonce contact card
-//so the Card component can be reused for other contacts.
-//4. Import the contacts.js file to create card components.
+//1. Apply CSS styles to App.jsx component CHECK
+//to match the appearance on the completed app: CHECK
+//https://c6fkx.csb.app/ CHECK
+//2. Extract the contact card as a reusable Card component. CHECK
+//3. Use props to render the default Beyonce contact card CHECK
+//so the Card component can be reused for other contacts.CHECK
+//4. Import the contacts.js file to create card components. CHECK
